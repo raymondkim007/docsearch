1eb6fc6af961d24a7dfb496afbe2a24cc4795ed6
Raymond Kim :D <raymondkim123456789@gmail.com>
2023-04-27 22:14:32 -0700

    Updates

--- DocSearchServer.java
+++ DocSearchServer.java
@@ -67,7 +67,7 @@ class DocSearchServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
-            return;
+            return; 
         }
 
         int port = Integer.parseInt(args[0]);
